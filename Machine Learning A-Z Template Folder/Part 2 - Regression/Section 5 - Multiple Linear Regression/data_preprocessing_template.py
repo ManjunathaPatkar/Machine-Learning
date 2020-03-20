4091957fc2d5ed3fbac68f10e7f9642847f630ae
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2020-03-20 10:00:45 +0530

    Multiple linear regression

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/data_preprocessing_template.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/data_preprocessing_template.py
@@ -6,9 +6,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 4].values
+
+#encoding independent variable state
+#from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+#labelencoder_X = LabelEncoder()
+#X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+#onehotencoder = OneHotEncoder(categorical_features = [3])
+#X = onehotencoder.fit_transform(X).toarray()
+
+
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+from sklearn.compose import ColumnTransformer
+ct = ColumnTransformer([("State", OneHotEncoder(), [3])], remainder = 'passthrough')
+X= ct.fit_transform(X)
+
+#avoiding the dummy variable trap
+X=X[:,1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -21,3 +37,21 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+
+#fitting multiple linear regression to the training set
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor.fit(X_train,y_train)
+
+
+#Predicting the test set results
+y_pred=regressor.predict(X_test)
+
+
+
+
+
+
+
+
